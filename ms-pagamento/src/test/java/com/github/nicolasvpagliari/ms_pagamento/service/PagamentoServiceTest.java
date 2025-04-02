01ed64571172e3e118d350f7a34c1f8f52609afe
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-02 08:55:30 -0300

    test: delete - Service

---
+++ ms-pagamento/src/test/java/com/github/nicolasvpagliari/ms_pagamento/service/PagamentoServiceTest.java
@@ -0,0 +1,56 @@
+package com.github.nicolasvpagliari.ms_pagamento.service;
+
+import com.github.nicolasvpagliari.ms_pagamento.repository.PagamentoRepository;
+import com.github.nicolasvpagliari.ms_pagamento.service.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class PagamentoServiceTest {
+
+    @InjectMocks
+    private PagamentoService service;
+
+    @Mock
+    private PagamentoRepository repository;
+
+    private Long existingId;
+    private Long nonExistingId;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 10L;
+
+        Mockito.when(repository.existsById(existingId)).thenReturn(true);
+        Mockito.when(repository.existsById(nonExistingId)).thenReturn(false);
+        Mockito.doNothing().when(repository).deleteById(existingId);
+    }
+
+    @Test
+    @DisplayName("delete deveria não fazer nada quando Id existe")
+    public void deleteShouldDoNothingWhenIdExists() {
+        Assertions.assertDoesNotThrow(
+                () -> {
+                    service.deletePagamento(existingId);
+                }
+        );
+    }
+
+    @Test
+    @DisplayName("delete deveria lançar exceção ResourceNotFoundException quando Id não existe")
+    public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNonExists() {
+        Assertions.assertThrows(ResourceNotFoundException.class,
+                () -> {
+                    service.deletePagamento(nonExistingId);
+                }
+        );
+    }
+}
