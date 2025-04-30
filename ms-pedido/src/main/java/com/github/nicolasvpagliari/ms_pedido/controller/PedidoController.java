a166ed1eddc863f22095cab0d9635b1f1991c4bf
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-30 08:22:55 -0300

    feat: PedidoController

---
+++ ms-pedido/src/main/java/com/github/nicolasvpagliari/ms_pedido/controller/PedidoController.java
@@ -0,0 +1,25 @@
+package com.github.nicolasvpagliari.ms_pedido.controller;
+
+import com.github.nicolasvpagliari.ms_pedido.dto.PedidoDTO;
+import com.github.nicolasvpagliari.ms_pedido.service.PedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pedidos")
+public class PedidoController {
+
+    @Autowired
+    private PedidoService service;
+
+    @GetMapping
+    public ResponseEntity<List<PedidoDTO>> getAllPedidos() {
+        List<PedidoDTO> list = service.findAllPedidos();
+        return ResponseEntity.ok(list);
+    }
+}
