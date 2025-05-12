6987ce7fdd471d2792113fc805988bee50b722fa
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-12 11:13:11 -0300

    feat: add eureka client on application.properties ms-pedido

--- ms-pedido/src/main/java/com/github/nicolasvpagliari/ms_pedido/MsPedidoApplication.java
+++ ms-pedido/src/main/java/com/github/nicolasvpagliari/ms_pedido/MsPedidoApplication.java
@@ -2,8 +2,11 @@ package com.github.nicolasvpagliari.ms_pedido;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+
+@EnableDiscoveryClient
 public class MsPedidoApplication {
 
 	public static void main(String[] args) {
